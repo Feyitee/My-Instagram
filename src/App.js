eb7fafb21eac2b2d71dce6ac3f35c865014338f8
Feyi <anjuwontobi99@gamil.com>
2022-08-31 13:00:16 +0100

    Created my Navigation session for my Instagram-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Nav from "./components/Nav";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Nav />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import instagramLogo from "../assets/instagram.png";
+
+const Nav = () => {
+  return (
+    <nav>
+      <button className="logo">
+        <img src={instagramLogo} alt="logo" />
+      </button>
+      <input type="text" className="search" placeholder="search" />
+      <span className="nav-links">
+        <button>
+          {" "}
+          <i className="fa fa-home" />
+        </button>
+        <button>
+          {" "}
+          <i className="fas fa-comment-alt" />
+        </button>
+        <button>
+          {" "}
+          <i className="fas fa-compass" />
+        </button>
+        <button>
+          {" "}
+          <i className="fas fa-hea  rt" />
+        </button>
+      </span>
+    </nav>
+  );
+};
+
+export default Nav;
