434078f29a3f7ac7c4783a1dba8cbdf749f5bf14
Feyi <anjuwontobi99@gamil.com>
2022-09-02 14:15:08 +0100

    Set up DexieJS and creating object stores for persisting data

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,27 +1,87 @@
+import { useState } from "react";
+import getPhotoUrl from "get-photo-url";
 import profileIcon from "../assets/profileIcon.svg";
+import { db } from "../dexie";
+import { useEffect } from "react";
 
 const Bio = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: "Tobi Feyisara Anjuwon",
+    about: "Learn to code",
+  });
+  const [editFormIsOpen, setEditFormIsOpen] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(profileIcon);
+
+  useEffect(() => {
+    const setDataFromDb = async () => {
+      const userDetailsFromDb = await db.bio.get("info");
+      const profilePhotoFromDb = await db.bio.get("profilePhoto");
+      userDetailsFromDb && setUserDetails(userDetailsFromDb);
+      profilePhotoFromDb && setProfilePhoto(profilePhotoFromDb);
+    };
+    setDataFromDb();
+  });
+
+  const updateDetails = async (e) => {
+    e.preventDefault();
+    const objectData = {
+      name: e.target.nameOfUser.value,
+      about: e.target.aboutUser.value,
+    };
+    setUserDetails(objectData);
+    await db.bio.put(objectData, "info");
+    setEditFormIsOpen(false);
+  };
+  const updatePhoto = async () => {
+    const newProfilePhoto = await getPhotoUrl("#profilePhotoInput");
+    setProfilePhoto(newProfilePhoto);
+    await db.bio.put(newProfilePhoto, "profilePhoto");
+  };
+
+  const editButton = (
+    <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
+  );
   const editForm = (
-    <form className="edit-bio-form">
-      <input type="text" id="" placeholder="Your name" />
-      <input type="text" id="" placeholder="About you" />
+    <form className="edit-bio-form" onSubmit={(e) => updateDetails(e)}>
+      <input type="text" id="" name="nameOfUser" placeholder="Your name" />
+      <input type="text" id="" name="aboutUser" placeholder="About you" />
       <br />
-      <button type="button" className="cancel-button">
+      <button
+        type="button"
+        className="cancel-button"
+        onClick={() => setEditFormIsOpen(false)}
+      >
         Cancel
       </button>
-      <button type="button">Save</button>
+      <button
+        type="submit"
+        onClick={(e) => {
+          {
+            setEditFormIsOpen(false);
+          }
+        }}
+      >
+        Save
+      </button>
     </form>
   );
+
   return (
     <session className="bio">
-      <div className="profile-photo" role="button" title="Click to edit photo">
-        <img src={profileIcon} alt="profile" />
-      </div>
+      <input type="file" accept="image/*" name="photo" id="profilePhotoInput" />
+      <label htmlFor="profilePhotoInput" onClick={updatePhoto}>
+        <div
+          className="profile-photo"
+          role="button"
+          title="Click to edit photo"
+        >
+          <img src={profilePhoto} alt="profile" />
+        </div>
+      </label>
       <div className="profile-info">
-        <p className="name">Anjuwon Tobi</p>
-        <p className="about">Learn to code</p>
-        <button>Edit</button>
-        {editForm}
+        <p className="name">{userDetails.name}</p>
+        <p className="about">{userDetails.about}</p>
+        {editFormIsOpen ? editForm : editButton}
       </div>
     </session>
   );
